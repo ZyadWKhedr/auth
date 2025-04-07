627be9ace4d4e841b2c312e155c9d15c834745e2
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-07 17:27:02 +0200

    added core

---
+++ lib/core/constants/app_keys.dart
@@ -0,0 +1,3 @@
+String supabaseUrl = 'https://kqquqwxnfmazfcdxwjnq.supabase.co';
+String supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxcXVxd3huZm1hemZjZHh3am5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMzY4OTMsImV4cCI6MjA1OTYxMjg5M30.8H88KoK-UoG26QGJIVSFz1guJcV1Bkm5yk6TRrPfUOQ';
---
+++ lib/core/utils/input_validator.dart
@@ -0,0 +1,13 @@
+class InputValidator {
+  static String? validateEmail(String? value){
+     if (value == null || value.isEmpty) return 'Email is required';
+    final regex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return regex.hasMatch(value) ? null : 'Invalid email format';
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) return 'Password is required';
+    if (value.length < 6) return 'Password must be at least 6 characters';
+    return null;
+  }
+}
