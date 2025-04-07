c08cd4b47b54e7226145a4d68a7f19c6e96d9295
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-07 17:43:18 +0200

    added di for auth repo impl

---
+++ lib/app/di.dart
@@ -0,0 +1,11 @@
+import 'package:auth/data/repositories/auth_repo_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../domain/repositories/auth_repository.dart';
+
+final supabaseProvider = Provider((ref) => Supabase.instance.client);
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepoImpl(ref.watch(supabaseProvider));
+});
